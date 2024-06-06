3732fe002ad1da8de5b18df7846120c0f2abb42b
Doanratruong <trongtuong2010@gmail.com>
2024-06-06 22:09:51 +0700

    commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rasp/catkin_ws/src'
+whitelisted_packages = 'navstack_pub'.split(';') if 'navstack_pub' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rasp/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/rasp/catkin_ws/devel;/opt/ros/noetic' != '' else []
